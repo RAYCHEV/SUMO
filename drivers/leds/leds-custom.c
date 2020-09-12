295169ba7796a111e0531537cdc9c71407558030
Petar Upinov <petar.upinov@gmail.com>
2020-09-12 22:51:21 +0300

    sumo: leds: migrate to character device driver
    Check and change right access to the driver:
    ls -la /dev/leds_custom
    sudo chmod 777 /dev/leds_custom
    
    The following command can be used with the driver:
    echo 1 > /dev/leds_custom
    dmesg
    [13820.998308] Kernel module driver: leds custom open.
    [13820.998716] Kernel module driver: leds custom close.
    
    cat < /dev/leds_custom
    dmesg
    [13836.974817] Kernel module driver: leds custom open.
    [13836.979481] Kernel module driver: leds custom close.

--- drivers/leds/leds-custom.c
+++ drivers/leds/leds-custom.c
@@ -5,7 +5,21 @@
 #include <linux/kernel.h>   /* Needed for KERN_INFO */
 #include <linux/gpio.h>     /* Needed for gpio pins */
 
+#include <linux/init.h>    /* Needed for the macros */
+#include <linux/version.h> /* Needed for check linux version for ioctl functions */
+#include <linux/errno.h>   /* Needed for check errors */
+#include <linux/device.h>  /* Needed for devices driver */
+#include <linux/cdev.h>    /* Needed for character devices */
+#include <linux/fs.h>      /* Needed for file operations */
+#include <linux/uaccess.h> /* Needed for user access to kernel */
+
 #define	GPIO26	26
+#define FIRST_MINOR 0
+#define MINOR_CNT   1
+
+static dev_t dev;
+static struct cdev c_dev;
+static struct class *cl;
 
 enum {
 	SET_OFF	=	0,
@@ -15,6 +29,27 @@ enum {
 enum {
 	INPUT	=	0,
 	OUTPUT	=	1,
+};
+
+static int leds_open(struct inode *i, struct file *f)
+{
+    printk(KERN_INFO "Kernel module driver: leds custom open.\n");
+    return 0;
+}
+
+static int leds_close(struct inode *i, struct file *f)
+{
+    printk(KERN_INFO "Kernel module driver: leds custom close.\n");
+    return 0;
+}
+
+static struct file_operations leds_custom_fops =
+{
+    .owner = THIS_MODULE,
+    .open = leds_open,
+    .release = leds_close,
+    .read = NULL,
+    .write = NULL
 };
 
 static void subscribe_gpios(void) {
@@ -39,6 +74,29 @@ static int __init init_module_leds_custom(void)
 {
     printk(KERN_INFO "Load kernel module: leds custom.\n");
     subscribe_gpios();
+
+    if (alloc_chrdev_region(&dev, FIRST_MINOR, MINOR_CNT, "leds_custom") < 0) {
+        return -1;
+    }
+    if ((cl = class_create(THIS_MODULE, "leds_custom_chardrv")) == NULL) {
+        unregister_chrdev_region(dev, MINOR_CNT);
+        printk(KERN_INFO "Error character device create!\n");
+        return -1;
+    }
+    if (device_create(cl, NULL, dev, NULL, "leds_custom") == NULL) {
+        class_destroy(cl);
+        unregister_chrdev_region(dev, MINOR_CNT);
+        printk(KERN_INFO "Error device create!\n");
+        return -1;
+    }
+    cdev_init(&c_dev, &leds_custom_fops);
+    if (cdev_add(&c_dev, dev, MINOR_CNT) == -1) {
+        device_destroy(cl, dev);
+        class_destroy(cl);
+        unregister_chrdev_region(dev, MINOR_CNT);
+        printk(KERN_INFO "Error adding character device!\n");
+        return -1;
+    }
     return 0;
 }
 
@@ -46,6 +104,11 @@ static void __exit cleanup_module_leds_custom(void)
 {
     unsubscribe_gpios();
     printk(KERN_INFO "Unload kernel module: leds custom.\n");
+
+    cdev_del(&c_dev);
+    device_destroy(cl, dev);
+    class_destroy(cl);
+    unregister_chrdev_region(dev, MINOR_CNT);
 }
 
 module_init(init_module_leds_custom);
