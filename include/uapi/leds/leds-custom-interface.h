ae525da28fd0896703f603f21f3b8c55dfb4ffda
Petar Upinov <petar.upinov@gmail.com>
2020-09-12 23:53:21 +0300

    sumo: leds: add init ioctl fops to character device driver
    Adds ioctl functionality for getting message from userspace application and
    switch led on or off.

---
+++ include/uapi/leds/leds-custom-interface.h
@@ -0,0 +1,18 @@
+/*
+ * leds-custom-interface.h
+ *
+ * The file is using for connection
+ * between userspace and kernel
+ */
+#ifndef _LEDS_CUSTOM_INTERFACE_
+#define _LEDS_CUSTOM_INTERFACE_
+
+#include <linux/ioctl.h>
+
+typedef struct {
+    int status;
+} query_arg_t;
+
+#define QUERY_SET_VARIABLES _IOW('q', 1, query_arg_t *)
+
+#endif /* _LEDS_CUSTOM_INTERFACE_ */
