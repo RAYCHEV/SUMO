550260179beaa6174e3c51042f190f3f6ecd9436
Petar Upinov <petar.upinov@gmail.com>
2020-10-18 21:13:13 +0300

    sumo: leds: prepare header to be used from userspace

--- include/uapi/leds/leds-custom-interface.h
+++ include/uapi/leds/leds-custom-interface.h
@@ -4,16 +4,16 @@
  * The file is using for connection
  * between userspace and kernel
  */
-#ifndef _LEDS_CUSTOM_INTERFACE_
-#define _LEDS_CUSTOM_INTERFACE_
+#ifndef _UAPI_LEDS_CUSTOM_INTERFACE_
+#define _UAPI_LEDS_CUSTOM_INTERFACE_
 
 #include <linux/ioctl.h>
 
 typedef struct {
-    int status;
+    __user int status;
 } query_arg_t;
 
 #define QUERY_SET_VARIABLES _IOW('q', 1, query_arg_t *)
 #define QUERY_GET_VARIABLES _IOR('q', 2, query_arg_t *)
 
-#endif /* _LEDS_CUSTOM_INTERFACE_ */
+#endif /* _UAPI_LEDS_CUSTOM_INTERFACE_ */
